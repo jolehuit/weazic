79a101269bc4a83521297b39d7d249f1f41fba66
whiteshrt <130084799+whiteshrt@users.noreply.github.com>
2023-11-03 15:27:57 +0100

    ok

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,16 +7,10 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [vitePreprocess({})],
 
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
-};
+		kit: {
+			adapter: adapter({
+				// see the 'Deployment configuration' section below
+			})
+		}
+	};
 export default config;
